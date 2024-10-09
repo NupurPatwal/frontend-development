c2611ac27732e4d6c27b5beb7e4075a3f7ac086d
Nupur Patwal <nupurpatwal@outlook.com>
2024-10-10 01:49:08 +0530

    basics 02

--- tutorial/chapter_01.js
+++ tutorial/chapter_01.js
@@ -1,7 +1,13 @@
+// let a = " krish "
 
+// console.log(a.length > 4 ? "sahi"  :"galat" );
 
-let a = " krish "
+// let str = 0;
+// let num = 0;
+// console.log(str / num);
+// let a;
+// let nalla = null;
+// console.log(a / nalla);
+// console.log( nalla/a);
 
-
-
-console.log(a.length > 4 ? "sahi"  :"galat" );
+let a = `${}`
